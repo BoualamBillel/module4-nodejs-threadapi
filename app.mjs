5e4828082d644e351eab0abc3bcd6d37fa95885b
Billel Boualam <boualambillelsofien@gmail.com>
2025-11-13 15:39:02 +0100

    first commit

--- app.mjs
+++ app.mjs
@@ -2,6 +2,7 @@ import { loadSequelize } from "./database.mjs";
 import express from "express";
 import cors from "cors";
 import bcrypt from "bcrypt";
+import { DataTypes } from "sequelize";
 
 /**
  * Point d'entrée de l'application
@@ -12,14 +13,43 @@ async function main() {
         const sequelize = await loadSequelize();
         const app = express();
 
+        // Creation des tables
+        const User = sequelize.define("user", {
+            username : DataTypes.TEXT,
+            email : DataTypes.TEXT,
+            password : DataTypes.TEXT
+        });
 
+        const Post = sequelize.define("post", {
+            title : DataTypes.TEXT,
+            content : DataTypes.TEXT,
+            userId : DataTypes.INTEGER
+        });
 
+        const Comment = sequelize.define("comment", {
+            content : DataTypes.TEXT,
+            userId : DataTypes.INTEGER,
+            postId : DataTypes.INTEGER
+        });
 
+        // Association
+        User.hasMany(Post, {foreignKey : "userId", as : "posts"});
+        Post.belongsTo(User);
 
+        Post.hasMany(Comment, {foreignKey : "postId", as : "comments"});
+
+        Comment.belongsTo(User);
+        Comment.belongsTo(Post);
+
+        
+        
+        
         app.listen(3000, () => {
             console.log("Serveur démarré sur http://localhost:3000");
         });
-
+        
+        // Sync
+        await sequelize.sync();
 
     } catch (error) {
         console.error("Error de chargement de Sequelize:", error);
--- database.mjs
+++ database.mjs
@@ -6,8 +6,12 @@ import { Sequelize, DataTypes } from "sequelize";
  */
 export async function loadSequelize() {
     try {
-        const sequelize = new Sequelize(/* ... */);
-        // ...
+        const sequelize = new Sequelize("thread-api", "root", "root", {
+            dialect : "mysql",
+            host : "127.0.0.1"
+        });
+        await sequelize.authenticate();
+        console.log("Connexion établie à la BDD avec succès !");
         return sequelize;
     } catch (error) {
         console.error(error);
