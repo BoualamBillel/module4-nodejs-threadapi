1187d64ac6f71491f2747f4aeb21d0583848b4f8
Billel Boualam <boualambillelsofien@gmail.com>
2025-11-13 15:52:53 +0100

    I implemented the createTable function, which creates tables for users, posts, and comments and also manages their associations.

---
+++ table.mjs
@@ -0,0 +1,38 @@
+import { Sequelize, DataTypes } from "sequelize";
+
+export function createTable(sequelize) {
+    try {
+        const User = sequelize.define("user", {
+            username: DataTypes.TEXT,
+            email: DataTypes.TEXT,
+            password: DataTypes.TEXT
+        });
+    
+        const Post = sequelize.define("post", {
+            title: DataTypes.TEXT,
+            content: DataTypes.TEXT,
+            userId: DataTypes.INTEGER
+        });
+    
+        const Comment = sequelize.define("comment", {
+            content: DataTypes.TEXT,
+            userId: DataTypes.INTEGER,
+            postId: DataTypes.INTEGER
+        });
+    
+        // Association
+        User.hasMany(Post, { foreignKey: "userId", as: "posts" });
+        Post.belongsTo(User);
+    
+        Post.hasMany(Comment, { foreignKey: "postId", as: "comments" });
+    
+        Comment.belongsTo(User);
+        Comment.belongsTo(Post);
+
+        return {User, Post, Comment};
+
+    } catch (error) {
+        console.log('Erreur lors de la création des tables : ', error);
+    }
+
+}
