a6036070972cf39601ed0af92416642a5c3a3af2
Billel Boualam <boualambillelsofien@gmail.com>
2025-11-14 10:23:42 +0100

    Implemented the get and delete methods of the CRUD.

--- Helpers/crud.mjs
+++ Helpers/crud.mjs
@@ -20,7 +20,7 @@ export async function addUser(User, username, email, password) {
 
 export async function addPost(Post, title, content, userId) {
     try {
-       await Post.create({
+        await Post.create({
             title: title,
             content: content,
             userId: userId
@@ -92,8 +92,50 @@ export async function getAllComments(Comment) {
 }
 
 
+export async function getUserById(User, userId) {
+    try {
+        const res = await User.findByPk(userId, {
+            attributes: { exclude: ['password'] },
+            include: [
+                { association: 'posts', include: [{ association: 'comments' }] }
+            ]
+        }
+        );
+        return JSON.stringify(res);
+    } catch (error) {
+        console.error(`Erreur de la récupération de l'utilisateur avec l'id (${userId}) : `, error);
+        throw error;
+    }
+}
+
+
 // Update
 
 
 
 // Delete
+export async function deletePostByID(Post, postId) {
+    try {
+        await Post.destroy({
+            where: {
+                id: postId
+            }
+        });
+    } catch (error) {
+        console.error(`Erreur lors de la suppression du post avec l'id (${postId}) : `, error);
+        throw error;
+    }
+}
+
+export async function deleteCommentById(Comment, commentId) {
+    try {
+        await Comment.destroy({
+            where: {
+                id: commentId
+            }
+        });
+    } catch (error) {
+        console.error(`Erreur lors de la suppression du commentaire avec l'id (${commentId}) : `, error);
+        throw error;
+    }
+}
