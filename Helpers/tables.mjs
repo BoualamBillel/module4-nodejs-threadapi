9053d0d73c721bb90ecd49adbcf330aa397bc77a
Billel Boualam <boualambillelsofien@gmail.com>
2025-11-14 13:27:40 +0100

    The createTable function was implemented to create SQL tables and establish associations between them.

---
+++ Helpers/tables.mjs
@@ -0,0 +1,40 @@
+import { Sequelize, DataTypes } from "sequelize";
+import { loadSequelize } from "./database.mjs";
+
+/**
+ * 
+ * @param {Sequelize} sequelize 
+ */
+export async function createTable(sequelize) {
+    try {
+        const User = sequelize.define("user", {
+            username: DataTypes.TEXT,
+            email: DataTypes.TEXT,
+            password: DataTypes.TEXT
+        });
+
+        const Post = sequelize.define("post", {
+            title: DataTypes.TEXT,
+            content: DataTypes.TEXT
+        });
+
+        const Comment = sequelize.define("comment", {
+            content: DataTypes.TEXT,
+        });
+
+        // Association
+        User.hasMany(Post, { foreignKey: "userId", as: "posts" });
+        Post.belongsTo(User);
+
+        Post.hasMany(Comment, { foreignKey: "postId", as: "comments" });
+
+        Comment.belongsTo(User);
+        Comment.belongsTo(Post);
+        await sequelize.sync({force:true});
+        return { User, Post, Comment };
+
+    } catch (error) {
+        console.error("Erreur lors de la création des tables SQL : ", error);
+        throw error;
+    }
+} 
