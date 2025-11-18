26204266cf95afb2a4913699e10b1d1fa90f6e2d
Billel Boualam <boualambillelsofien@gmail.com>
2025-11-18 15:21:58 +0100

    Implemented all routes specified in the specifications

---
+++ Routes/postRoutes.mjs
@@ -0,0 +1,107 @@
+import express from "express";
+import { createTable } from "../Helpers/tables.mjs";
+import { loadSequelize } from "../Helpers/database.mjs";
+import { verifyToken } from "../auth/jwtMiddleware.mjs";
+
+
+const sequelize = await loadSequelize();
+const { Post } = await createTable(sequelize);
+const { Comment } = await createTable(sequelize);
+
+const router = express.Router();
+
+router.post("/posts", verifyToken, async (req, res) => {
+    try {
+        const userId = req.user.id;
+
+        const post = await Post.create({
+            "title": req.body.title,
+            "content": req.body.content,
+            "userId": userId
+        });
+        res.status(201).json(post);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Erreur serveur" });
+    }
+});
+
+router.get("/posts", async (req, res) => {
+    try {
+        const posts = await Post.findAll({
+            include: [{ association: "comments" }]
+        });
+
+        res.status(200).json(posts);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Erreur serveur" });
+    }
+});
+
+router.post("posts/:postId/comments", verifyToken, async (req, res) => {
+    try {
+        if (!req.body || !req.body.content) {
+            return res.status(400).json({ message: "Un commentaire doit avoir un contenu" });
+        }
+        if (!req.body.postId) {
+            return res.status(400).json({ message: "Post ID manquant" })
+        }
+        const comment = await Comment.create({
+            "content": req.body.content,
+            "postId": req.params.postId,
+            "userId": req.user.id
+        });
+        res.status(201).json(comment);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Erreur serveur" });
+    }
+});
+
+router.delete("/posts/:postId", verifyToken, async (req, res) => {
+    try {
+        const deleted = await Post.destroy({ where: { id: req.params.postId } });
+        if (deleted) {
+            res.status(200).json({ message: "Post supprimé avec succès" });
+        } else {
+            res.status(404).json({ message: "Erreur lors de la suppresion du post" });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Erreur serveur" });
+    }
+});
+
+router.delete("/comments/:commentId", verifyToken, async (req, res) => {
+    try {
+        const deleted = await Comment.destroy({ where: { id: req.params.commentId } });
+        if (deleted) {
+            res.status(200).json({ message: "Commentaire supprimé avec succès" });
+        } else {
+            res.status(404).json({ message: "Erreur lors de la suppresion du commentaire" });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Erreur serveur" });
+    }
+});
+
+router.get("/users/:userId/posts", async (req, res) => {
+    try {
+        const userPosts = await Post.findAll({
+            where: { userId: req.params.userId }
+        });
+        if (userPosts.length > 0) {
+            res.status(200).json(userPosts);
+        } else {
+            res.status(404).json({message: "Erreur lors de la récupération des posts de l'utilisateur"});
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Erreur serveur" });
+    }
+});
+
+export default router;
+
